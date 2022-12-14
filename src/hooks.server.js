723cd16c1e3600ce42f142441b0d55bf7c1e5b68
gq <guille121093@gmail.com>
2022-12-13 22:06:25 -0300

    ADDED: hooks.server.js fixed

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,16 +1,13 @@
 import PocketBase from 'pocketbase';
 
+export const handle = async ({ event, resolve }) => {
+	event.locals.pb = new PocketBase('http://localhost:8090');
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
-export const handle = async ({event, resolve}) => {
-  event.locals.pb = new PocketBase('http://localhost:8090');
-  event.locals.pb.authStore.loadFromCookie(event.request.headers.get('Cookie') || '');
-
-  if (event.locals.pb.authStore.isValid) {
-    event.locals.user = event.locals.pb.authStore.model;
-  }
-
-  const response = await resolve(event);
-
-  response.headers.set('Set-Cookie', event.locals.pb.authStore.cookie);
-
-}
+	if (event.locals.pb.authStore.isValid) {
+		event.locals.user = event.locals.pb.authStore.model;
+	}
+	const response = await resolve(event);
+	response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }));
+	return response;
+};
