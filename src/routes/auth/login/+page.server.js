48e9a910cbdb2f441873ba7b671d0f290667bece
gq <guille121093@gmail.com>
2022-12-21 21:59:30 -0300

    nabvar started

--- src/routes/auth/login/+page.server.js
+++ src/routes/auth/login/+page.server.js
@@ -1,38 +1,44 @@
-import { redirect } from "@sveltejs/kit";
-import { serializeNonPOJOs } from "$lib/helpers.js";
+import { redirect } from '@sveltejs/kit';
+import { serializeNonPOJOs } from '$lib/helpers.js';
 
+//La funcion load se ejecuta antes de que se renderice la pagina
 export const load = ({ locals }) => {
-  if (locals.pb.authStore.isValid) {
-    throw redirect(303, '/index/main');
-  }
+	//si el usuario ya esta autenticado, redireccionamos a la ruta de inicio
+	if (locals.pb.authStore.isValid) {
+		console.log('Usuario ya autenticado, redireccionando...');
+		throw redirect(303, '/index/main');
+	}
 };
 
 export const actions = {
-  //metodo asincrono para iniciar sesion
-  login: async ({ locals, request }) => {
-    //obtenemos los datos del formulario
-    const formData = await request.formData();
-    //creamos un objeto con los datos del formulario y lo serializamos
-    const data = serializeNonPOJOs(Object.fromEntries([...formData]));
-    console.log('Datos de formulario: ', data);
-    try {
-      //iniciamos sesion
-      await locals.pb.collection('users').authWithPassword(
-        data.email,
-        data.password
-      );
-      console.log('Sesion iniciada con exito');
-      console.log('Valid: ',locals.pb.authStore.isValid);
-      console.log('Token: ',locals.pb.authStore.token);
-      console.log('ID: ',locals.pb.authStore.model.id);
-    } catch (err) {
-      console.log('Error:', err);
-      return {
-        error: true,
-        message: err
-      };
-    }
-    //redireccionamos a la ruta de inicio si no hay errores
-    throw redirect(303, '/index/main');
-  }
-}
+	//metodo asincrono para iniciar sesion
+	login: async ({ locals, request }) => {
+		//obtenemos los datos del formulario
+		const formData = await request.formData();
+
+		//creamos un objeto con los datos del formulario y lo serializamos
+		const data = serializeNonPOJOs(Object.fromEntries([...formData]));
+		console.log('Datos de formulario: ', data);
+
+		//iniciamos sesion
+		try {
+			await locals.pb.collection('users').authWithPassword(data.email, data.password);
+
+			//logs de control
+			console.log('Sesion iniciada con exito');
+			console.log('Valid: ', locals.pb.authStore.isValid);
+			console.log('Token: ', locals.pb.authStore.token);
+			console.log('ID: ', locals.pb.authStore.model.id);
+			console.log('Email: ', locals.pb.authStore.model.email);
+		} catch (err) {
+			console.log('Error:', err);
+			return {
+				error: true,
+				message: err
+			};
+		}
+
+		//redireccionamos a la ruta de inicio si no hay errores
+		throw redirect(303, '/index/main');
+	}
+};
--- src/routes/index/main/+page.svelte
+++ src/routes/index/main/+page.svelte
@@ -1,3 +1,91 @@
-<form method="POST" action="?/logout">
-  <button id="button"  >Cerrar sesión</button>
-</form>
+<!--NavBar-->
+
+<div class="navbar bg-primary text-primary-content">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<label tabindex="0" class="btn btn-ghost btn-circle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h7"
+					/></svg
+				>
+			</label>
+			<ul
+				tabindex="0"
+				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<li><a>Homepage</a></li>
+				<li><a>Portfolio</a></li>
+				<li><a>About</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="navbar-center">
+		<a class="btn btn-ghost normal-case text-xl">SmartDrive.</a>
+	</div>
+	<div class="navbar-end">
+		<label for="my-modal-3" class="btn btn-ghost btn-circle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/></svg
+			>
+		</label>
+
+		<div class="dropdown dropdown-end">
+			<label  tabindex="0" class="btn btn-ghost avatar">
+				<div class="w-10 mask mask-squircle">
+					<img src="https://placeimg.com/192/192/people" />
+				</div>
+			</label>
+			<ul
+				tabindex="0"
+				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<li>
+					<a class="justify-between">
+						Perfil
+						<span class="badge">New</span>
+					</a>
+				</li>
+				<li><a>Ajustes</a></li>
+				<li>
+					<form method="POST" action="?/logout">
+						<button id="button">Cerrar sesión</button>
+					</form>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<!-- Search Modal -->
+<input type="checkbox" id="my-modal-3" class="modal-toggle" />
+<div class="modal">
+	<div class="modal-box relative">
+		<label
+			for="my-modal-3"
+			class="btn btn-sm btn-circle border-none bg-base-100 text-black font-bold absolute right-2 top-2"
+			>✕</label
+		>
+		<div class="form-control pr-6">
+			<input type="text" placeholder="Buscar" class="input input-bordered bg-base-100 " />
+		</div>
+	</div>
+</div>
